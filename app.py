1e1a83727c1d541ad7571a512d2d8f42f8fe9209
Selproider <selenamolgar2012@gmail.com>
2026-08-01 20:16:07 +0200

    inicio.html añadido
    El archivo "inicio.html" lo añadí, a parte de que el botón "Comenzar" ahora es funcional y te lleva al inicio.html

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# Index.html
 @app.route("/")
-def inicio():
+def index():
     return render_template("index.html")
+
+
+# Inicio.html
+@app.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
